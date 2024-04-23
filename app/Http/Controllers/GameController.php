0777a6dbbcdc4e97e7b3e6e0de0249a2ce40da07
VíctorBlasco5 <victor.blasco.17@gmail.com>
2024-04-23 12:37:21 +0200

    feat: get all games with model

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -2,13 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Game;
 use Illuminate\Http\Request;
 
 class GameController extends Controller
 {
     public function getAllGames()
     {
-        return 'GET ALL GAMES';
+        try {
+            $games = Game::all();
+
+            return response()->json(
+                [
+                    "success" => true,
+                    "message" => "Games retrieved successfully",
+                    "data" => $games
+                ],
+                200
+            );
+        } catch (\Throwable $th) {
+            return response()->json(
+            [
+                "success" => false,
+                "message" => "Games cant be retrieved successfully",
+                "error" => $th->getMessage()
+            ],
+            500
+        );
+        }
     }
 
     public function createGames(Request $request) {
